ec09b9802e5ddd52f0e1272f0f07652fe94a9f16
n1c fury <n1c@0x01.local>
2023-01-18 18:48:37 -0800

    added additional comments

--- furiousrecon.sh
+++ furiousrecon.sh
@@ -5,7 +5,7 @@ YLW=$'\e[1;33m'
 GRN=$'\e[1;32m'
 BLU=$'\e[1;34m'
 WTE=$'\e[0m'
-echo "";
+echo ""; #Fancy Banner
 echo "$YLW  ================================================";
 echo "$BLU[*]	FuriousRecon - @n1c_Fury                 [*]";
 echo "$BLU[*]	Usage: ./recon.sh <ip> <name>	         [*]";
@@ -19,9 +19,9 @@ echo "$WTE[*] Notes are in ${BLU}$2-report.txt";
 echo "$WTE[*] Access the $2-report.html to review the report";
 echo "$WTE[*] Check the web page here: ${BLU}http://$1 ";
 mkdir $2;
-mkdir $2/{nmap-output,tools};
+mkdir $2/{nmap-output,tools,images}; #Additional subfolders for your target
 cd $2;
-echo "$GRN[*] Creating HTML report file...";
+echo "$GRN[*] Creating HTML report..."; #Creation of HTML page
 echo "<!DOCTYPE html>" > $2-report.html;
 echo "<html>" >> $2-report.html;
 echo "<title>[*] Host Report for $1 - $2 [*]</title> " >> $2-report.html;
@@ -40,7 +40,7 @@ echo "</h3>" >> $2-report.html;
 echo "</nav>" >> $2-report.html;
 echo "</body>" >> $2-report.html;
 echo "</html>" >> $2-report.html;
-echo "$YLW[*] Starting scans....";
+echo "$YLW[*] Starting scans...."; #nmap scans ran here. Adjust accordingly
 echo "$WTE[1] IppSec Initial Scan";
 nmap -sC -sV -oA nmap-output/ippsec $1 1> /dev/null;
 sleep 5;
@@ -65,23 +65,23 @@ echo "$WTE[6] Running UDP Scan...";
 nmap -Pn -T4 -sS -sU -v -v -oA nmap-output/udp $1 1> /dev/null;
 sleep 5;
 xsltproc nmap-output/udp.xml -o nmap-output/udp.html;
-echo "$GRN[*] Creating Report Template...";
+echo "$GRN[*] Creating Report Note file..."; #Creates txt file for findings
 echo "[+] IP/Host: $1 - $2" >> $2-report.txt;
 echo "[+] TCP/UDP Ports:" >> $2-report.txt;
 echo "$YLW[*] Grabbing Ports...";
 cat nmap-output/allports.nmap |grep "/" |cut -d " " -f 1 > portlist.txt;
 cat nmap-output/udp.nmap |grep "/" |cut -d " " -f 1 >> portlist.txt;
 cat portlist.txt |sort -n |uniq >> $2-report.txt;
-echo "[+] Web Recon" >> $2-report.txt;
-echo "[+] User Creds discovered" >> $2-report.txt;
-echo "[+] Host Enumeration" >> $2-report.txt;
-echo "[+] Network Connections" >> $2-report.txt;
-echo "[+] Attack Surface" >> $2-report.txt;
-echo "[+] Possible Vulnerabilties" >> $2-report.txt;
-echo "[+] Effective Exploits" >> $2-report.txt;
-echo "[+] Gaining a Foothold" >> $2-report.txt;
-echo "[+] Foothold to Shell" >> $2-report.txt;
-echo "[+] Privilege Escalation to Root" >> $2-report.txt;
+echo "[+] Web Recon (directories, robots.txt, etc)" >> $2-report.txt;
+echo "[+] User Creds discovered (did you find users and/or passwords" >> $2-report.txt;
+echo "[+] Network Connections (e.g. netstat output)" >> $2-report.txt;
+echo "[+] Attack Surface (what's in the box and what versions)" >> $2-report.txt;
+echo "[+] Possible Vulnerabilties (these might work)" >> $2-report.txt;
+echo "[+] Effective Exploits (these did work)" >> $2-report.txt;
+echo "[+] Gaining a Foothold (how you found a way in)" >> $2-report.txt;
+echo "[+] Foothold to Shell (reproduce steps for your shell)" >> $2-report.txt;
+echo "[+] Host Enumeration (what's on the box)" >> $2-report.txt;
+echo "[+] Priv Esc to Root (how you got from shell to root)" >> $2-report.txt;
 echo "[+] proof.txt" >> $2-report.txt;
 echo "user: " >> $2-report.txt;
 echo "root: " >> $2-report.txt;
