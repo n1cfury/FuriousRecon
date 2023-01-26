1268da978e1248f9711a705a297e9c5da14f8edd
n1cfury <info@n1cfury.com>
2023-01-25 21:48:21 -0800

    Added code for staging.

--- furiousrecon.py
+++ furiousrecon.py
@@ -5,12 +5,11 @@ import os
 import nmap
 import lxml
 import sys
+import glob
 
 '''
 Furious Recon Todos
     Make working (root) folder e.g. target name
-    Make subfolders (images, tools, nmap-output) 
-    Create HTML page inside working folder (lines 25-42)
     Nmap scans (lines 45-67)
         foreach [scan,5 sec delay,xsltproc]
     Create Report txt file (lines 68-87)
@@ -21,8 +20,6 @@ What are the most important functions
     create txt file for report notes
     Run multiple nmap scans (output all formats)
 
-Modules needed
-nmap, os, lxml, glob
 '''
 #Variables
 thost = (sys.argv(1))
@@ -43,6 +40,57 @@ vln='-Pn -T4 -v -v --script=*enum* -oA nmap-output/enum'
 udp='-Pn -T4 -sS -sU -v -v -oA nmap-output/udp'
 scans = [ipc,all,svc,enm,vln]
 
+html_code = """
+<!DOCTYPE html>
+<html>" 
+<title>[*] Host Report for %s - %s [*]</title> % 
+<body>
+<h2 align="center"><b>[*] $2 - $1 [*]</h></bold>
+<nav>
+<h3 align="center">
+<a href="nmap-output/ippsec.html" target="iframe_a">IppSec</a>
+<a href="nmap-output/allports.html" target="iframe_a">All Ports</a>
+<a href="nmap-output/service.html" target="iframe_a">Services</a>
+<a href="nmap-output/enum.html" target="iframe_a">Enum</a>
+<a href="nmap-output/vulns.html" target="iframe_a">Vulns</a>
+<a href="nmap-output/udp.html" target="iframe_a">UDP</a>
+<p align="left"><iframe height="800px" width="800px" name="iframe_a"></iframe></p>
+</h3>
+</nav>
+</body>
+</html>
+"""
+
+report= '''
+[+] IP/Host:
+[+] TCP/UDP Ports:
+[+] Web Recon (directories, robots.txt, etc)
+[+] User Creds discovered (did you find users and/or passwords
+[+] Network Connections (e.g. netstat output)
+[+] Attack Surface (what's in the box and what versions)
+[+] Possible Vulnerabilties (these might work)
+[+] Effective Exploits (these did work)
+[+] Gaining a Foothold (how you found a way in)
+[+] Foothold to Shell (reproduce steps for your shell)
+[+] Host Enumeration (what's on the box)
+[+] Priv Esc to Root (how you got from shell to root)
+[+] proof.txt
+user: 
+root: 
+[+] Post Exploitation
+'''
+
+def staging(): #Make target folders and files, and change directory into target folder
+    with open("index.html", "w") as file:
+        file.write(html_code)
+    with open("report.txt", "w") as file:
+        file.write(report)
+    if not os.path.exists(tfolder):
+        os.makedirs(tfolder)
+        for sub in subfolders:
+            os.makedirs(sub)
+    os.chdir(tfolder)
+
 def banner(): #Fancy banner for the tool
     print ("")
     print ("[*] ================================================[*]")
@@ -57,12 +105,6 @@ def usage(): #Prints usage of the tool
     print ("Usage: ./furiousrecon.sh <ip> <name>")
     print ("example: ./furiousrecon 192.168.5.5 targetfolder ")
     sys.exit()
-
-def staging(): #Make target directory and subdirectories
-        if not os.path.exists(tfolder):
-        os.makedirs(tfolder)
-        for sub in subfolders:
-            os.makedirs(sub)
 
 def recon(): #Runs nmap scans
     for scan in scans:
